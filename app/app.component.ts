ecd6450ba33874d8c84acf71cb696004638327b7
Damyan Petev <damyan.petev@gmail.com>
2016-05-16 17:10:14 +0300

    adding characters json, model and module barrel
    switching typings to local to avoid failing installs without global one

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
+import {  } from "./characters";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>GoT Characters Helper</h1>'
 })
 export class AppComponent { }
---
+++ app/characters/characters.model.ts
@@ -0,0 +1,8 @@
+/**
+ * Character
+ */
+export class Character {
+    name: string;
+    popName: string;
+    dead: boolean = false;
+}
